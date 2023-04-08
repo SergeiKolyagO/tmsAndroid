bac3830b37b34402d73e679703ab78a34ea6338e
SergeiKolyagO <trus_m@mail.ru>
2023-04-08 20:16:23 +0300

    Kolyago

--- app/src/main/java/com/example/homework/Info.kt
+++ app/src/main/java/com/example/homework/Info.kt
@@ -5,14 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class Info : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_info)
     }
-
     fun onClick(view: View) {
-        val intent = Intent(this,Info::class.java)
+        val intent = Intent(this,MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         startActivity(intent)
     }
 }
